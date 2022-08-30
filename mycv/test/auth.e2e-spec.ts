3f9c7272bd5dc086fbde7067592b869e80341c70
Tobias Schotenröhr <tis@denios.de>
2022-08-30 14:56:04 +0200

    Section 14: Managing App Configuration

--- mycv/test/auth.e2e-spec.ts
+++ mycv/test/auth.e2e-spec.ts
@@ -5,6 +5,7 @@ import { AppModule } from './../src/app.module';
 
 describe('Authentication System', () => {
   let app: INestApplication;
+  const email = 'cdfsdfdssdg@test.com';
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -16,7 +17,6 @@ describe('Authentication System', () => {
   });
 
   it('handles a signup request', () => {
-    const email = 'cdfsdfdssdg@test.com';
     return request(app.getHttpServer())
       .post('/auth/signup')
       .send({ email, password: '123456' })
@@ -26,5 +26,21 @@ describe('Authentication System', () => {
         expect(id).toBeDefined();
         expect(email).toEqual(email);
       });
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: '123456' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
   });
 });
